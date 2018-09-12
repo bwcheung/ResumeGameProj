1a68b72ce7b1e26a65c36fe8def4f216f0ea8b12
bwcheung <bwcheung@ualberta.ca>
2018-09-12 17:40:14 -0600

    Update guy.js

--- src/guy.js
+++ src/guy.js
@@ -23,9 +23,9 @@ export default class sprite {
 	}
 		
         
-    render(state) {
+   	render(state) {
     	
-    	var frameIndex = this.frameIndex;
+    		var frameIndex = this.frameIndex;
 		var positionX = this.positionX;
 		var positionY = this.positionY;
 		var nf = this.numberOfFrames;
@@ -39,45 +39,44 @@ export default class sprite {
 		ctx.clearRect(positionX ,positionY, width, height);
 		
 		if (!state.endGame && state.gameStart) {
-    		if (state.keys.right && (this.positionX < state.screen.width / 2)) {
-        		this.positionX += 10;
-        	}
-        	
-        	if (state.keys.up && (this.positionY > 0)) {
-        		this.positionY -= 10;
-        	}
-        	
-        	if (state.keys.down && (this.positionY < state.screen.height)) {
-        		this.positionY += 10;   		
-        	}
-        	
-        	if (state.keys.left && (this.positionX > 0)) {
-        		this.positionX -= 10;
-        	}
-        	
-        	if ((state.keys.space) && (Date.now() - this.lastShot > 600)) { 	
-        		this.frameIndex = 1;
-        		const bullet = new Bullet({guy:this});
-        		this.bullets(bullet);
-        		this.lastShot = Date.now();
-        	} else {
-        		this.frameIndex = 0;
-        	}
-    	}
-    	//ctx.clearRect(positionX ,positionY, width, height);
-    	
+			if (state.keys.right && (this.positionX < state.screen.width / 2)) {
+				this.positionX += 10;
+			}
+
+			if (state.keys.up && (this.positionY > 0)) {
+				this.positionY -= 10;
+			}
+
+			if (state.keys.down && (this.positionY < state.screen.height)) {
+				this.positionY += 10;   		
+			}
+
+			if (state.keys.left && (this.positionX > 0)) {
+				this.positionX -= 10;
+			}
+
+			if ((state.keys.space) && (Date.now() - this.lastShot > 600)) { 	
+				this.frameIndex = 1;
+				const bullet = new Bullet({guy:this});
+				this.bullets(bullet);
+				this.lastShot = Date.now();
+			} else {
+				this.frameIndex = 0;
+			}
+    		}
+		
 		newImage.src = this.image;
 		newImage.onload = function () {
-			  ctx.drawImage(
-						newImage,
-						frameIndex * width / nf,
-					    0,
-					    width / nf,
-					    height,
-					    positionX,
-					    positionY,
-					    width / nf,
-					    height);
+			ctx.drawImage(
+				newImage,
+				frameIndex * width / nf,
+				0,
+				width / nf,
+				height,
+				positionX,
+				positionY,
+				width / nf,
+				height);
 			  
 		 };
 		 ctx.restore();
